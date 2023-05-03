af5f5084dc8048267fd1514bfef66d6595cdc448
Shotunde1 <Kehindeshotunde367@gmail.com>
2023-05-03 15:48:12 -0700

    Bread

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - update the value of n to 98
- * @n: input
- * Return: n
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
